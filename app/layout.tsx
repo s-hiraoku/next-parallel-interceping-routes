4b4fbbdb8b46731b66e4d6ebc61444b145d0db1b
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-07-25 10:04:57 +0900

    create analytics page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <div className="h-screen flex flex-col">
           <header className="bg-blue-700 text-white py-4 shadow-md">
             <div className="container mx-auto px-4">
-              <h1 className="text-3xl font-bold">My Application</h1>
+              <h1 className="text-3xl font-bold">Animalytics</h1>
             </div>
           </header>
           <main className="flex-1 container mx-auto px-4 py-6">
