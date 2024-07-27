ec046646ef64c941638dc1b32f42cd84565cc6b9
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-07-27 12:45:37 +0900

    add utils.

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+import { type ReactNode } from "react";
+
+/**
+ * 表示されるReactNode
+ *
+ * `ReactNode` から `true`, `false`, `null`, `undefined` を抜いたもの
+ */
+type RenderableReactNode = Exclude<ReactNode, null | undefined | boolean>;
+
+/**
+ * 表示されない React node (`true`, `false`, `null`, `undefined`) の時に `false` を、それ以外は `true` を返す
+ *
+ * @see https://react.dev/reference/react/isValidElement#react-elements-vs-react-nodes:~:text=true%2C%20false%2C%20null%2C%20or%20undefined%20(which%20are%20not%20displayed)
+ */
+export const isRenderableReactNode = (
+  node: ReactNode
+): node is RenderableReactNode =>
+  node !== undefined && node !== null && typeof node !== "boolean";
