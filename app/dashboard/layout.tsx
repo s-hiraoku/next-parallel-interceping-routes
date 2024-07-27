410e7c672ec2e886b621bc72a6f15c01ebf5bf91
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-07-27 17:01:00 +0900

    Change component architecture.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,31 +7,14 @@ export default function DashboardLayout({
   analytics: React.ReactNode;
   team: React.ReactNode;
 }) {
-  const hasAnalytics = analytics != null;
-  const hasTeam = team != null;
-
   return (
     <div className="h-full flex flex-col bg-gray-100 p-4">
       <div className="mb-4 bg-white shadow-md p-4 rounded-md">{children}</div>
       <div className="flex gap-4 h-full min-h-0">
-        {hasAnalytics && (
-          <div
-            className={`flex-1 bg-white shadow-md p-4 rounded-md ${
-              !hasTeam ? "w-full" : ""
-            }`}
-          >
-            {analytics}
-          </div>
-        )}
-        {hasTeam && (
-          <div
-            className={`flex-1 bg-white shadow-md p-4 rounded-md ${
-              !hasAnalytics ? "w-full" : ""
-            }`}
-          >
-            {team}
-          </div>
-        )}
+        <div className={"flex-1 bg-white shadow-md p-4 rounded-md"}>
+          {analytics}
+        </div>
+        <div className={"flex-1 bg-white shadow-md p-4 rounded-md"}>{team}</div>
       </div>
     </div>
   );
