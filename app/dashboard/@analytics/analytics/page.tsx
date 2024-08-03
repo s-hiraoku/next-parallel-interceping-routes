47d2fef17021148de207bd7bf700a4c4c7ee6088
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-08-03 14:25:30 +0900

    add Loading component.

--- app/dashboard/@analytics/analytics/page.tsx
+++ app/dashboard/@analytics/analytics/page.tsx
@@ -1,9 +1,11 @@
-import { AnalyticsData } from "@/app/_components/AnalyticsData";
+// import { AnalyticsData } from "@/app/_components/AnalyticsData";
+import { Loading } from "@/app/_components/Loading";
 
 export default function AnalyticsPage() {
-  return (
-    <div className="h-full w-full bg-blue-50 p-6 overflow-auto">
-      <AnalyticsData />
-    </div>
-  );
+  // return (
+  //   <div className="h-full w-full bg-blue-50 p-6 overflow-auto">
+  //     <AnalyticsData />
+  //   </div>
+  // );
+  return <Loading />;
 }
