b0a79a62c65164a0b6195d9a9bf1f1d794cb0f6b
hiraoku.shinichi <s.hiraoku@gmail.com>
2024-08-03 14:28:55 +0900

    fix Analytics page

--- app/dashboard/@analytics/analytics/page.tsx
+++ app/dashboard/@analytics/analytics/page.tsx
@@ -1,11 +1,9 @@
-// import { AnalyticsData } from "@/app/_components/AnalyticsData";
-import { Loading } from "@/app/_components/Loading";
+import { AnalyticsData } from "@/app/_components/AnalyticsData";
 
 export default function AnalyticsPage() {
-  // return (
-  //   <div className="h-full w-full bg-blue-50 p-6 overflow-auto">
-  //     <AnalyticsData />
-  //   </div>
-  // );
-  return <Loading />;
+  return (
+    <div className="h-full w-full bg-blue-50 p-6 overflow-auto">
+      <AnalyticsData />
+    </div>
+  );
 }
